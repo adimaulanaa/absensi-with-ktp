07ebc236be658131f6239dfdbdae47bad1139009
usradmho <usradmho@Digital-Automation---MacBook-Air.local>
2025-02-12 10:25:38 +0700

    fix view all employee attendance

--- lib/features/dashboard/presentation/pages/view_all_employee.dart
+++ lib/features/dashboard/presentation/pages/view_all_employee.dart
@@ -11,6 +11,7 @@ import 'package:attendance_ktp/features/dashboard/presentation/bloc/dashboard_st
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:intl/intl.dart';
 
 class ViewAllEmployee extends StatefulWidget {
   const ViewAllEmployee({super.key});
@@ -22,6 +23,7 @@ class ViewAllEmployee extends StatefulWidget {
 class _ViewAllEmployeeState extends State<ViewAllEmployee> {
   final searchController = TextEditingController();
   List<EmployeeModel> allEmployee = [];
+  List<EmployeeModel> employee = [];
 
   @override
   void initState() {
@@ -33,7 +35,7 @@ class _ViewAllEmployeeState extends State<ViewAllEmployee> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: AppColors.bgScreen,
+      backgroundColor: AppColors.bgGreySecond,
       appBar: AppBar(
         backgroundColor: AppColors.bgScreen,
         centerTitle: true,
@@ -74,6 +76,7 @@ class _ViewAllEmployeeState extends State<ViewAllEmployee> {
           } else if (state is EmployeeLoaded) {
             if (state.data.isNotEmpty) {
               allEmployee = state.data;
+              employee = allEmployee;
             }
           }
         },
@@ -104,77 +107,89 @@ class _ViewAllEmployeeState extends State<ViewAllEmployee> {
         padding: const EdgeInsets.only(left: 15, right: 15),
         child: Column(
           children: [
-            SizedBox(height: size.height * 0.05),
+            SizedBox(height: size.height * 0.01),
             Padding(
-              padding: const EdgeInsets.only(left: 5, right: 5),
-              child: TextFormField(
-                controller: searchController,
-                decoration: InputDecoration(
-                  hintText: 'Search Note...',
-                  hintStyle: greyTextstyle.copyWith(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white, // Kotak warna putih
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      blurRadius: 5,
+                      offset: const Offset(0, 3), // Bayangan bawah
+                    ),
+                  ],
+                ),
+                child: TextFormField(
+                  controller: searchController,
+                  decoration: InputDecoration(
+                    hintText: 'Search Note...',
+                    hintStyle: greyTextstyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: light,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: const BorderSide(color: Colors.blue, width: 1.5),
+                    ),
+                    filled: true,
+                    fillColor: Colors.white, // Pastikan fillColor juga putih
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: SvgPicture.asset(
+                        MediaRes.dSearch,
+                        // ignore: deprecated_member_use
+                        color: AppColors.bgGrey,
+                        width: 20,
+                      ),
+                    ),
+                    suffixIcon: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: InkWell(
+                        splashFactory: NoSplash.splashFactory,
+                        highlightColor: Colors.transparent,
+                        onTap: () {
+                          searchController
+                              .clear(); // Menghapus teks saat diklik
+                        },
+                        child: SvgPicture.asset(
+                          MediaRes.dSearchRight,
+                          // ignore: deprecated_member_use
+                          color: AppColors.bgGrey,
+                          width: 20,
+                        ),
+                      ),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 12.0, horizontal: 14.0),
+                  ),
+                  onChanged: (value) {
+                    search(value);
+                  },
+                  maxLines: 1,
+                  style: blackTextstyle.copyWith(
                     fontSize: 16,
                     fontWeight: light,
                   ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      color: AppColors.bgTrans,
-                      width: 1.0,
-                    ),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  focusedBorder: InputBorder.none,
-                  filled: true,
-                  fillColor: AppColors.bgColor,
-                  // Menambahkan ikon di kiri
-                  prefixIcon: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: SvgPicture.asset(
-                      MediaRes.dSearch, // Ganti dengan ikon kiri yang sesuai
-                      // ignore: deprecated_member_use
-                      color: AppColors.bgGrey,
-                      width: 20, // Sesuaikan ukuran ikon
-                    ),
-                  ),
-                  // Ikon di kanan tetap ada
-                  suffixIcon: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: InkWell(
-                      splashFactory: NoSplash.splashFactory,
-                      highlightColor: Colors.transparent,
-                      onTap: () {
-                        // searchController.text = '';
-                        // search('');
-                      },
-                      child: SvgPicture.asset(
-                        MediaRes.dSearchRight,
-                        // ignore: deprecated_member_use
-                        color: AppColors.bgGrey,
-                        width: 20, // Sesuaikan ukuran ikon
-                      ),
-                    ),
-                  ),
-                  contentPadding: const EdgeInsets.symmetric(
-                      vertical: 12.0, horizontal: 14.0),
-                ),
-                onChanged: (value) {
-                  // search(value);
-                },
-                maxLines: 1,
-                style: blackTextstyle.copyWith(
-                  fontSize: 16,
-                  fontWeight: light,
                 ),
               ),
             ),
             SizedBox(height: size.height * 0.05),
             SingleChildScrollView(
               child: Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20),
+                padding: const EdgeInsets.only(left: 10, right: 10),
                 child: Column(
-                  children: allEmployee.map((e) {
+                  children: employee.map((e) {
                     return listEmployee(size, e);
                   }).toList(),
                 ),
@@ -185,40 +200,89 @@ class _ViewAllEmployeeState extends State<ViewAllEmployee> {
       ),
     );
   }
+
+  void search(String query) {
+    final lowerCaseQuery = query.toLowerCase(); // Pencarian berdasarkan nama
+
+    List<EmployeeModel> init = allEmployee;
+    employee = [];
+    setState(() {
+      employee = init.where((e) {
+        final applicantName = e.name!.toLowerCase(); // Nama pelamar
+
+        // Pencocokan query dengan nama pelamar
+        bool matchesQuery = applicantName.contains(lowerCaseQuery);
+
+        return matchesQuery;
+      }).toList();
+    });
+  }
 }
 
 Widget listEmployee(
   Size size,
   EmployeeModel e,
 ) {
+  String formattedDate = DateFormat('dd MMMM yyyy').format(e.createdOn!);
+  String formattedTime = DateFormat('HH:mm:ss').format(e.createdOn!);
   return Container(
-    height: size.height * 0.05,
-    padding: const EdgeInsets.only(left: 20, right: 20),
+    // height: size.height * 0.05,
+    padding: const EdgeInsets.only(left: 20, right: 20, top: 5, bottom: 5),
     decoration: BoxDecoration(
       color: Colors.green,
       borderRadius: BorderRadius.circular(8),
     ),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Text(
-          e.id.toString(),
-          style: whiteTextstyle.copyWith(
-            fontSize: 12,
-            fontWeight: medium,
-          ),
-          overflow: TextOverflow.ellipsis,
-          maxLines: 1,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              e.id.toString(),
+              style: whiteTextstyle.copyWith(
+                fontSize: 12,
+                fontWeight: medium,
+              ),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+            ),
+            const SizedBox(width: 5),
+            Text(
+              e.name.toString(),
+              style: whiteTextstyle.copyWith(
+                fontSize: 12,
+                fontWeight: medium,
+              ),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+            ),
+          ],
         ),
-        const SizedBox(width: 5),
-        Text(
-          e.name.toString(),
-          style: whiteTextstyle.copyWith(
-            fontSize: 12,
-            fontWeight: medium,
-          ),
-          overflow: TextOverflow.ellipsis,
-          maxLines: 1,
+        const SizedBox(height: 5),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              formattedDate,
+              style: whiteTextstyle.copyWith(
+                fontSize: 12,
+                fontWeight: medium,
+              ),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+            ),
+            const SizedBox(width: 5),
+            Text(
+              formattedTime,
+              style: whiteTextstyle.copyWith(
+                fontSize: 12,
+                fontWeight: medium,
+              ),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+            ),
+          ],
         ),
       ],
     ),
